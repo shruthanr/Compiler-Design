a5bfc87025f940c179873e3146cb9e134cb45168
niranjansy <niranjan.sy99@gmail.com>
2020-09-06 01:45:46 +0530

    Add parser code and test cases

---
+++ Parser/y.tab.h
@@ -0,0 +1,123 @@
+/* A Bison parser, made by GNU Bison 3.0.4.  */
+
+/* Skeleton interface for Bison GLR parsers in C
+
+   Copyright (C) 2002-2015 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    NO_ELSE = 258,
+    ELSE = 259,
+    GE_OP = 260,
+    LE_OP = 261,
+    EQ_OP = 262,
+    NE_OP = 263,
+    IDENTIFIER = 264,
+    STRING_CONSTANT = 265,
+    CHAR_CONSTANT = 266,
+    INT_CONSTANT = 267,
+    FLOAT_CONSTANT = 268,
+    SIZEOF = 269,
+    INC_OP = 270,
+    DEC_OP = 271,
+    LEFT_OP = 272,
+    RIGHT_OP = 273,
+    AND_OP = 274,
+    OR_OP = 275,
+    MUL_ASSIGN = 276,
+    DIV_ASSIGN = 277,
+    MOD_ASSIGN = 278,
+    ADD_ASSIGN = 279,
+    SUB_ASSIGN = 280,
+    LEFT_ASSIGN = 281,
+    RIGHT_ASSIGN = 282,
+    AND_ASSIGN = 283,
+    XOR_ASSIGN = 284,
+    OR_ASSIGN = 285,
+    TYPE_NAME = 286,
+    DEF = 287,
+    CHAR = 288,
+    SHORT = 289,
+    INT = 290,
+    LONG = 291,
+    SIGNED = 292,
+    UNSIGNED = 293,
+    FLOAT = 294,
+    DOUBLE = 295,
+    CONST = 296,
+    VOID = 297,
+    IF = 298,
+    WHILE = 299,
+    CONTINUE = 300,
+    BREAK = 301,
+    RETURN = 302,
+    EndOfFile = 303,
+    UNARY = 304
+  };
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+
+union YYSTYPE
+{
+#line 19 "parser.y" /* glr.c:197  */
+
+        long int4;              /* Constant integer value */
+        float fp;               /* Constant floating point value */
+        char *str;              /* Ptr to constant string (strings are malloc'd) */
+    
+
+#line 111 "y.tab.h" /* glr.c:197  */
+};
+
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
