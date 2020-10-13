052ce534847849b86424ecd080a88bc6691951e0
niranjansy <niranjan.sy99@gmail.com>
2020-10-13 15:56:10 +0530

    Add semantic analyzer code

---
+++ Semantic Analyzer/test2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int arr2[10];
+
+int trial(int b, int c, int d)
+{
+	int a;
+	int arr1[10];
+
+	return 1;
+}
+
+int main()
+{
+	int a;
+	int k=5;
+	int arr[10];
+	long int z;
+	while(1)
+	{
+		int b=5;
+		while(1)
+		{
+			int c;
+			int arr3[10];
+		}
+	}
+	arr = 10;
+}
